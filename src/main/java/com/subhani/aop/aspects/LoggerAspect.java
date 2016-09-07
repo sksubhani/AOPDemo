eec0bd5594a08a7150e0515820ed81f4643597e1
sksubhani <sk_subha@yahoo.com>
2016-09-06 18:03:43 -0700

    Added pointcut methods

--- src/main/java/com/subhani/aop/aspects/LoggerAspect.java
+++ src/main/java/com/subhani/aop/aspects/LoggerAspect.java
@@ -3,11 +3,22 @@ package com.subhani.aop.aspects;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggerAspect {
+	
+	/*
+	@Pointcut("execution(* getFirstName(..))")
+	private void logForNameAttributes() {}
+	
+	@Before("logForNameAttributes")
+	public void logForNames() {
+		System.out.println("@Pointcut ASPECT RUNNING ....");
+	}
+	*/
 	
 	@Before("execution (* *Employee(..) )")
 	public void logBefore() {
@@ -17,5 +28,5 @@ public class LoggerAspect {
 	@After("execution (* *Employee(..) )")
 	public void logAfter() {
 		System.out.println("@After ASPECT RUNNING ....");
-	}
+	}	
 }
